deaccb592fcd719fbf9efc4004054fa1f266dfad
Reis <gabrielreisn@gmail.com>
2020-10-31 17:15:56 -0300

    Finalização da limpeza com remoção dos duplicados que faltavam

--- limpeza.R
+++ limpeza.R
@@ -4,7 +4,6 @@ library(readxl)
 library(tidyverse)
 library(lubridate)
 library(stringr)
-library(chron) # formato hora e minuto
 library(abjutils) # remover acentos
 library(janitor)
 library(stringi)
@@ -31,8 +30,7 @@ historico_limpo <- historico %>%
             turma = as.factor(Turma),
             mencao = factor(Menção, ordered = TRUE, levels = c("SR", "II", "MI", "TR", "TJ", "DP", "CC", "MM", "MS", "SS")),
             faltas = as.numeric(str_replace(Faltas, "%", ""))/100,
-            hora_inicio = times(paste0(`Hora início`, ":00")),
-            hora_fim = times(paste0(`Hora Fim`, ":00")),
+            horario = factor(paste0(`Hora início`,'-', `Hora Fim`), ordered = TRUE),
             resultado = fct_collapse(mencao,
                                      "Aprovação" = c("SS", "MS", "MM", "CC", "DP"),
                                      "Reprovação" = c("MI", "II", "SR"),
@@ -75,15 +73,15 @@ table(historico_limpo$curso) # Aparentemente existe curso de computação (sem s
 historico_limpo %>% count(nome, disciplina, ano, periodo) %>% filter(n > 1) %>% nrow # 18 mil casos
 historico_limpo %>% get_dupes(nome, disciplina, ano, periodo)
 
-historico_limpo %>% count(nome, disciplina, ano, periodo, hora_inicio, hora_fim) %>% filter(n > 1) %>% nrow # 14 mil
-historico_limpo %>% get_dupes(nome, disciplina, ano, periodo, hora_inicio, hora_fim)
+historico_limpo %>% count(nome, disciplina, ano, periodo, horario) %>% filter(n > 1) %>% nrow # 14 mil
+historico_limpo %>% get_dupes(nome, disciplina, ano, periodo, horario)
 
 historico_limpo %>% count(nome, curso, disciplina, ano, periodo) %>% filter(n > 1) %>% nrow # 12 mil
 
-historico_limpo %>% get_dupes(nome, disciplina, ano, periodo, hora_inicio, hora_fim)
-historico_limpo %>% count(nome, curso, disciplina, ano, periodo, hora_inicio, hora_fim) %>% filter(n > 1) %>% nrow # 6 mil
-historico_limpo %>% count(nome, curso, disciplina, professor, ano, periodo, hora_inicio, hora_fim) %>% filter(n > 1) %>% nrow # 2 mil
-historico_limpo %>% count(nome, curso, disciplina, professor, ano, periodo, hora_inicio, hora_fim) %>% filter(n > 1) %>% nrow # 2 mil
+historico_limpo %>% get_dupes(nome, disciplina, ano, periodo, horario)
+historico_limpo %>% count(nome, curso, disciplina, ano, periodo, horario) %>% filter(n > 1) %>% nrow # 6 mil
+historico_limpo %>% count(nome, curso, disciplina, professor, ano, periodo, horario) %>% filter(n > 1) %>% nrow # 2 mil
+historico_limpo %>% count(nome, curso, disciplina, professor, ano, periodo, horario) %>% filter(n > 1) %>% nrow # 2 mil
 
 # Aparentemente tem muitas observações que repetem pela quantidade de créditos (tem valores repetidos até 10 vezes)
 # Substituir nome por matricula reduz os casos repetidos, provavelmente porque a pessoa trocou de curso/matricula
@@ -93,13 +91,30 @@ historico_limpo <- historico_limpo %>% distinct(nome, disciplina, ano, periodo, 
 # Importante: tinha deixado antes a matricula como string, aí na hora de remover duplicadas, não removeu a certa "1100102410" < "9913246"
 
 # Menções Diferentes
-historico_limpo %>% arrange(desc(mencao)) %>% get_dupes(nome, matricula, disciplina, curso, ano, periodo, hora_inicio, hora_fim)
+historico_limpo %>% arrange(desc(mencao)) %>% get_dupes(nome, matricula, disciplina, curso, ano, periodo, horario)
 
 historico_limpo <- historico_limpo %>% 
   arrange(desc(mencao)) %>% 
-  distinct(nome, matricula, disciplina, curso, ano, periodo, hora_inicio, hora_fim, .keep_all = TRUE)
+  distinct(nome, matricula, disciplina, curso, ano, periodo, horario, .keep_all = TRUE)
 
-# #TODO ainda tem repetição pra só horarios diferentes 
+# Horarios Diferentes
+levels(historico_limpo$horario)
+
+historico_limpo <- historico_limpo %>% 
+  mutate(horario = fct_collapse(horario,
+                                "08:00-09:50" = c("08:00-09:50", "08:00-10:00"),
+                                "14:00-15:50" = c("14:00-15:50", "14:00-16:00"),
+                                "16:00-17:50" = c("16:00-17:50", "16:00-18:00"),
+                                "19:00-20:50" = c("19:00-20:50", "19:00-20:40"),
+                                "20:50-22:30" = c("20:50-22:30", "20:50-22:40"),
+                                "14:00-17:40" = c("14:00-17:40", "14:00-16:45", "14:00-17:00"))) # ultimos dois só tem 1 obs
+
+historico_limpo %>% get_dupes(nome, disciplina, ano, periodo) # 36
+historico_limpo %>% get_dupes(nome, disciplina, ano, periodo, matricula) # 0
+
+historico_limpo <- historico_limpo %>% 
+  arrange(matricula) %>% 
+  distinct(nome, disciplina, ano, periodo, .keep_all = TRUE)
 
 saveRDS(historico_limpo, "historico_limpo.rds")
   
@@ -114,7 +129,7 @@ vagas_limpo <- vagas %>%
     ano = as.integer(str_sub(Semestre, 1, 4)),
     periodo = as.factor(str_sub(Semestre, -1, -1)),
     disciplina_cod = as.factor(`Cód. Disciplina`),
-    disciplina = as.factor(rm_accent(toupper(Disciplina))),
+    disciplina = as.factor(rm_accent(stri_trans_totitle(Disciplina))),
     quant_alunos_matriculados_ajuste = as.integer(`Qtde de Alunos que Conseguiram Vaga (Etapa de Ajuste)`),
     quant_alunos_nao_matriculados_ajuste = as.integer(`Quantitativo de alunos que não conseguiram se matricular por falta de vagas`)
   )
@@ -123,8 +138,23 @@ str(vagas_limpo)
 
 table(vagas_limpo$disciplina)
 
+servico <- c("Bioestatistica", "Estatistica Aplicada", "Probabilidade E Estatistica", "Probabilidade E Estatistica 2")
+
 vagas_limpo <- vagas_limpo %>% 
-  mutate(disciplina = str_replace(disciplina, "DAS SERIES TEMPORAIS", "DE SERIES TEMPORAIS"))
+  mutate(disciplina = str_replace(disciplina, "Das Series Temporais", "De Series Temporais"),
+         disciplina = fct_collapse(disciplina, "Estatstica Exploratoria" = c("Estatistica Exploratoria 1", "Estatistica Exploratoria")),
+         tipo = as.factor(ifelse(disciplina %in% servico, "Serviço", "Bacharelado")))
+
+# Colocar acento
+
+vagas_limpo <- vagas_limpo %>%
+  mutate(disciplina = str_replace_all(disciplina, c("tistica" = "tística", "Analise" = "Análise", "coes" = "ções", 
+                                                    "cao" = "ção", "Series" = "Séries", "encia" = "ência",
+                                                    "tistico" = "tístico", "tagio" = "tágio", "Historia" = "História", 
+                                                    "Met" = "Mét", "casticos" = "cásticos", "Item" = "Ítem",
+                                                    "Topicos" = "Tópicos", "sao" = "são", "Exploratoria" = "Exploratória")))
+
+str(vagas_limpo)
 
 table(vagas_limpo$disciplina)
 
